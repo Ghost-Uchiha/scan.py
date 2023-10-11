7e8ad850a7c53cfd0c5866cb76a5f8145b5de07c
Nirav Jangale <120158935+Ghost-Uchiha@users.noreply.github.com>
2023-10-11 16:54:00 +0530

    Add files via upload

---
+++ scan.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, jsonify
+from keras.models import load_model
+import tensorflow as tf
+import numpy as np
+from pymongo import MongoClient
+from PIL import Image
+import io
+from decouple import config
+
+app = Flask(__name__)
+
+@app.route('/process_image', methods=['GET'])
+def process_image():
+    MONGODB_URI = config('Mongo_URL')
+    client = MongoClient(MONGODB_URI)
+    db = client['test']
+    collection = db['images']
+    model_file_path = "path_to_your_model.h5"  # Update with the correct path
+    model = load_model(model_file_path)
+
+    latest_image = collection.find_one(sort=[('_id', -1)])
+    image_data = latest_image['img']['data']
+    img = Image.open(io.BytesIO(image_data))
+
+    try:
+        img = img.resize((224, 224))
+        img_tensor = tf.keras.utils.img_to_array(img)
+        img_tensor = np.expand_dims(img_tensor, axis=0)
+        img_tensor /= 255
+
+        pred = model.predict(img_tensor)
+        result = pred[0][0] * 100
+
+        if result:
+            return jsonify({'result': result})
+        else:
+            return jsonify({'message': 'Congratulations, You Are Safe'})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
